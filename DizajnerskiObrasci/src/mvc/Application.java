45f2544376824b977a13a64ca04b1d8faf9ffdba
HP ProBook 430 G2 <HP ProBook 430 G2@DESKTOP-OHKSD1E>
2023-12-06 17:28:49 +0100

    Initial commit

--- DizajnerskiObrasci/src/mvc/Application.java
+++ DizajnerskiObrasci/src/mvc/Application.java
@@ -4,6 +4,7 @@ public class Application {
 
 	public static void main(String[] args) {
 		System.out.println("Dobrodošli na vežbe iz predmeta Dizajnerski obrasci.");
+		System.out.println("Nevena Majmunka");
 		
 		DrawingModel model = new DrawingModel();
 		DrawingFrame frame = new DrawingFrame();
